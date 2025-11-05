488e92769f15b8c595d4618ca9e645a72a28601e
fidel-ernesto <fidelmaster16@gmail.com>
2025-11-05 23:15:32 +0000

    agregamos el puerto como variable de ambiente

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ const badPath= (request, response, next) => {
 
 app.use(badPath)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`Server running in port ${PORT}`);
